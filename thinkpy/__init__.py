62ab3bbd4ffd64ecd345d64a5d1779205fcd980e
Larry Gray <lwgray@gmail.com>
2024-11-16 22:46:00 -0700

    Change versioning

--- thinkpy/__init__.py
+++ thinkpy/__init__.py
@@ -6,5 +6,5 @@ from .parser import parse_thinkpy
 from .interpreter import ThinkPyInterpreter
 from . import jupyter_magic
 
-__version__ = "2.0.0"
+__version__ = "0.1.9"
 __all__ = ["parse_thinkpy", "ThinkPyInterpreter", "jupyter_magic"]
